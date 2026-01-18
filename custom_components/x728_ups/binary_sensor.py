7d0ea56c34bc83eb3e16670606b852d80fb089be
psopso <57403373+psopso@users.noreply.github.com>
2026-01-18 08:54:54 +0100

    Update binary_sensor.py

--- custom_components/x728_ups/binary_sensor.py
+++ custom_components/x728_ups/binary_sensor.py
@@ -1,20 +1,19 @@
 from homeassistant.components.binary_sensor import BinarySensorEntity
-from .const import DOMAIN
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data["x728_ups"][entry.entry_id]
-    add_entities([X728PowerLoss(coordinator)])
+    async_add_entities([X728PowerLoss(coordinator)])
 
-class X728PowerLoss(BinarySensorEntity):
+
+class X728PowerLoss(CoordinatorEntity, BinarySensorEntity):
     _attr_name = "X728 Power Loss"
     _attr_device_class = "power"
+    _attr_unique_id = "x728_power_loss"
 
     def __init__(self, coordinator):
-        self.coordinator = coordinator
+        super().__init__(coordinator)
 
     @property
     def is_on(self):
         return bool(self.coordinator.data.get("power_loss"))
-
-    async def async_update(self):
-        await self.coordinator.async_request_refresh()
