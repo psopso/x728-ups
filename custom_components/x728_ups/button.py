157f76872f8508b5010e4fa7ce53efd9949f5036
psopso <57403373+psopso@users.noreply.github.com>
2026-01-18 08:55:44 +0100

    Update button.py

--- custom_components/x728_ups/button.py
+++ custom_components/x728_ups/button.py
@@ -1,16 +1,18 @@
 from homeassistant.components.button import ButtonEntity
-from .const import DOMAIN
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data["x728_ups"][entry.entry_id]
-    add_entities([X728ShutdownButton(coordinator)])
+    async_add_entities([X728ShutdownButton(coordinator)])
 
-class X728ShutdownButton(ButtonEntity):
+
+class X728ShutdownButton(CoordinatorEntity, ButtonEntity):
     _attr_name = "X728 Shutdown Host"
     _attr_icon = "mdi:power"
+    _attr_unique_id = "x728_shutdown_button"
 
     def __init__(self, coordinator):
-        self.coordinator = coordinator
+        super().__init__(coordinator)
 
     async def async_press(self):
         await self.coordinator.shutdown_host()
